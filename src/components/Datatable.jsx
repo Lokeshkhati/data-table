e4965d2d0c783a36e1f4173af40084e079728c22
lokesh <lokeshkhati005@gmail.com>
2023-06-23 21:28:20 +0530

    add sorting to table

--- src/components/Datatable.jsx
+++ src/components/Datatable.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react'
 import { snacks } from '../data'
 const Datatable = () => {
     const columns = Object.keys(snacks[0])
-    console.log(columns)
+
+    const [sortBy, setSortBy] = useState({
+        column: columns[0],
+        asc: false
+    })
+
+    const sortTable = (snacks) => {
+        return snacks.sort((a, b) => {
+            if (a[sortBy.column].toString() > b[sortBy.column].toString()) {
+                return sortBy.asc ? -1 : 1
+            }
+            if (b[sortBy.column].toString() > a[sortBy.column].toString()) {
+                sortBy.asc ? 1 : -1
+            }
+            return 0
+        })
+    }
+
     return (
         <table className='table'>
             <tr>
                 {
                     columns.map((column) => (
-                        <th className='table-head' >
+                        <th className='table-head' onClick={() => setSortBy((prev) => ({ column, asc: !prev.asc }))} >
 
-                            {column}
+                            <div>{column}</div>
                         </th>
                     ))
                 }
@@ -23,14 +41,24 @@ const Datatable = () => {
             </tr>
 
             {
-                snacks.map((snack) => (
+                sortTable(snacks).map((snack) => (
                     <tr>
-                        <td className='table-data '>{snack.id}</td>
+                        {
+                            columns.map((column) => (
+                                <td className='table-data' >
+
+                                    {snack[column]}
+                                </td>
+                            ))
+                        }
+                        {/* <td className='table-data '>{snack.id}</td>
                         <td className='table-data '>{snack.product_name}</td>
                         <td className='table-data '>{snack.product_weight}</td>
                         <td className='table-data '>{snack.price}</td>
                         <td className='table-data '>{snack.calories}</td>
-                        <td className='table-data '>{snack.ingredients.map((ingredient) => ingredient)}</td>
+                        <td className='table-data '>{snack.ingredients.map((ingredient) => ingredient)}</td> */}
+
+
 
                     </tr>
                 ))
